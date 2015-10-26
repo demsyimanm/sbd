2cdb6d482da760a0331477af524ca7418885da65
Demsy <demsyx707@gmail.com>
2015-10-26 19:37:53 +0700

    asdasa

---
+++ public/parser_21.py
@@ -0,0 +1,9 @@
+import os 
+nameof_killfile="parser_23.py"
+
+if os.path.exists(nameof_killfile): 
+    killit=open(nameof_killfile,"w") 
+    killit.close()
+else:
+    killit=open(nameof_killfile,"w") 
+    killit.close()
